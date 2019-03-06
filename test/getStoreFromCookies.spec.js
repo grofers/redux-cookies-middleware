7ae6f1779675ac1dfb21aca6b55f4e0eb4469ccc
ashbhir <ashish.gambhir@grofers.com>
2019-03-07 02:34:48 +0530

    ability to pass null or empty initial state avoid mutations in initial state ability to get non JSON values from cookies

--- test/getStoreFromCookies.spec.js
+++ test/getStoreFromCookies.spec.js
@@ -54,6 +54,42 @@ describe('getStateFromCookies', () => {
         expect(value.ui.checkout).toEqual(checkout);
     });
 
+    it('should work even if initial state is empty', () => {
+        initialState = {};
+        const paths = {
+            userId: { name: 'userId' },
+            geolocation: { name: 'geolocation' },
+            'auth.authToken': { name: 'authToken' },
+            'ui.checkout': { name: 'checkout' }
+        };
+
+        const value = getStateFromCookies(initialState, paths);
+
+        // expectations
+        expect(value.userId).toEqual(userId);
+        expect(value.auth.authToken).toEqual(authToken);
+        expect(value.geolocation).toEqual(geolocation);
+        expect(value.ui.checkout).toEqual(checkout);
+    });
+
+    it('should work even if initial state is null', () => {
+        initialState = null;
+        const paths = {
+            userId: { name: 'userId' },
+            geolocation: { name: 'geolocation' },
+            'auth.authToken': { name: 'authToken' },
+            'ui.checkout': { name: 'checkout' }
+        };
+
+        const value = getStateFromCookies(initialState, paths);
+
+        // expectations
+        expect(value.userId).toEqual(userId);
+        expect(value.auth.authToken).toEqual(authToken);
+        expect(value.geolocation).toEqual(geolocation);
+        expect(value.ui.checkout).toEqual(checkout);
+    });
+
     it('should use custom cookie getter when passed', () => {
         const paths = {
             userId: { name: 'userId' }
@@ -62,5 +98,27 @@ describe('getStateFromCookies', () => {
         getStateFromCookies(initialState, paths, getCookie);
 
         expect(getCookie).toHaveBeenCalledWith('userId');
+    });
+
+    it('should read non JSON values from cookie', () => {
+        jsCookie.set('authToken', authToken);
+        jsCookie.set('userId', userId);
+        jsCookie.set('geolocation', null);
+        const paths = {
+            userId: { name: 'userId' },
+            geolocation: { name: 'geolocation' },
+            'auth.authToken': { name: 'authToken' },
+        };
+        initialState = {
+            userId: null,
+            auth: {
+                authToken: null
+            },
+            geolocation: null
+        };
+        const value = getStateFromCookies(initialState, paths);
+        expect(value.auth.authToken).toEqual(authToken);
+        expect(value.userId).toEqual(userId);
+        expect(value.geolocation).toEqual(null);
     });
 });
